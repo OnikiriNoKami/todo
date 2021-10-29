f3a803f1b0d2735e51e84c8e56e3448e386a9fb1
OnikiriNoKami <butenko.ily@gmail.com>
2021-10-29 21:50:07 +0300

    user schema usage

--- backend/src/api/graphql/root.js
+++ backend/src/api/graphql/root.js
@@ -1,48 +1,8 @@
 const { mergeSchemas } = require("@graphql-tools/schema");
-const { GraphQLObjectType, GraphQLString, GraphQLList, GraphQLSchema, GraphQLNonNull } = require('graphql')
 const statusSchema = require('./schema/statusSchema.js');
-
-
-// const todoQueryes = new GraphQLObjectType({
-//     name: 'Queries',
-//     fields: {
-//         todos: {
-//             type: new GraphQLList(todoSchema),
-//             resolve: () => {
-//                 return [{title: 'Poop', description: 'Peep'}]
-//             }
-//         }
-//     }
-// })
-
-// const randomQuery = new GraphQLObjectType({
-//     name: 'Queries',
-//     fields: {
-//         pendehps: {
-//             type: new GraphQLList(todoSchema),
-//             resolve: ()=>{
-//                 return [{title: 'Poop', description: 'Peep'},{title: 'Poop', description: 'Peep'}]
-//             }
-//         }
-//     }
-// })
-
-// const randomMutation = new GraphQLObjectType({
-//     name: 'Mutations',
-//     fields: {
-//         create: {
-//             type: todoSchema,
-//             args: {
-//                 title: {type: new GraphQLNonNull(GraphQLString)}
-//             },
-//             resolve: (_, {title})=> {
-//                 return {title, descriotion: 'new object'}
-//             }
-//         }
-//     }
-// })
+const { userSchema } = require('./schema/userSchema');
 
 const rootSchema = mergeSchemas({
-    schemas:[statusSchema]
+    schemas:[statusSchema, userSchema]
 })
 module.exports = rootSchema;
