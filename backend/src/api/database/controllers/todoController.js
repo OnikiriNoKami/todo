e5ca420c3793de429ecf32c73073aa309dcc518e
OnikiriNoKami <butenko.ily@gmail.com>
2021-10-30 23:17:55 +0300

    todo creation and data retrieving

---
+++ backend/src/api/database/controllers/todoController.js
@@ -0,0 +1,58 @@
+const Todo = require("../schemas/todoSchema");
+const { findAndAddTodo } = require("./userController");
+
+const todoController = {
+    create: async ({
+        title = "Default title",
+        description = "",
+        userId = "",
+        statusId = "",
+        beginDate = "",
+        endDate = "",
+        withTodos,
+    }) => {
+        try{
+            const todo = new Todo({title, description, userId, statusId, beginDate, endDate});
+            await todo.save()
+            await findAndAddTodo({_id: userId, todoId: todo._id, withTodos})
+
+            return todo
+
+        } catch (error){
+            console.log(error.message)
+            return error
+        }
+    },
+
+    getAllTodos: async () => {
+        try {
+            const result = await Todo.find()
+            return result
+        } catch (error){
+            console.log(error.message)
+            return error
+        }
+    },
+
+    getAllTodosWithUser: async () => {
+        try {
+            const result = await Todo.aggregate([
+                {
+                    $lookup: {
+                        from: 'users',
+                        localField: 'userId',
+                        foreignField: '_id',
+                        as: 'user'
+                      },
+                },
+            ]);
+            return result || null;
+
+        } catch (error){
+            console.log(error.message)
+            return error
+        }
+    }
+};
+
+module.exports = todoController;
