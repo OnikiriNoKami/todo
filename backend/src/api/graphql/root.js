8b911617db2490907cf1c6a519bad85fbb9b4656
OnikiriNoKami <butenko.ily@gmail.com>
2021-10-30 23:22:48 +0300

    todo schema root integration

--- backend/src/api/graphql/root.js
+++ backend/src/api/graphql/root.js
@@ -1,8 +1,9 @@
 const { mergeSchemas } = require("@graphql-tools/schema");
 const statusSchema = require('./schema/statusSchema.js');
 const { userSchema } = require('./schema/userSchema');
+const { todoSchema } = require('./schema/todoSchema')
 
 const rootSchema = mergeSchemas({
-    schemas:[statusSchema, userSchema]
+    schemas:[statusSchema, userSchema, todoSchema]
 })
 module.exports = rootSchema;
