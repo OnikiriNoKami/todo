596459b76c7921e4f7a7229d293bf4fe7d3d4ade
OnikiriNoKami <butenko.ily@gmail.com>
2021-10-31 12:23:33 +0200

    comment schema integration

--- backend/src/api/graphql/root.js
+++ backend/src/api/graphql/root.js
@@ -1,9 +1,10 @@
 const { mergeSchemas } = require("@graphql-tools/schema");
 const statusSchema = require('./schema/statusSchema.js');
 const { userSchema } = require('./schema/userSchema');
-const { todoSchema } = require('./schema/todoSchema')
+const { todoSchema } = require('./schema/todoSchema');
+const { commentSchema } = require('./schema/commentSchema');
 
 const rootSchema = mergeSchemas({
-    schemas:[statusSchema, userSchema, todoSchema]
+    schemas:[statusSchema, userSchema, todoSchema, commentSchema]
 })
 module.exports = rootSchema;
