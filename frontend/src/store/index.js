9d4a1d92e9bc8bed9fbd0f79be40b8755515f0d7
OnikiriNoKami <butenko.ily@gmail.com>
2021-10-31 16:07:42 +0200

    root store for redux

---
+++ frontend/src/store/index.js
@@ -0,0 +1,10 @@
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { thunk } from "redux-thunk";
+
+const combinedReducer = combineReducers({});
+
+export const store = createStore(
+    combinedReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
