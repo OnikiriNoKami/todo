4080af8b68dce15b77a534a77f8edb780d750495
OnikiriNoKami <butenko.ily@gmail.com>
2021-11-01 19:40:41 +0200

    user reducer

---
+++ frontend/src/store/user/userReducer.js
@@ -0,0 +1,41 @@
+import userActionsTypes from "../../utils/reduxActionType/userActionsTypes";
+
+const defaultState = {
+    email: "",
+    phone: "",
+    nickName: "",
+    todos: [],
+    token: "",
+};
+
+const userReducer = (state = defaultState, { type, payload }) => {
+    switch (type) {
+        case userActionsTypes.SET_USER_NICKNAME:
+            return { ...state, nickName: payload };
+        case userActionsTypes.SET_USER_EMAIL:
+            return { ...state, email: payload };
+        case userActionsTypes.SET_USER_PHONE:
+            return { ...state, phone: payload };
+        case userActionsTypes.SET_USER_TODOS:
+            return { ...state, todos: payload };
+        case userActionsTypes.SET_USER_TOKEN:
+            return { ...state, token: payload };
+        case userActionsTypes.SET_USER_DATA:
+            return {
+                email: payload.email || state.email,
+                phone: payload.phone || state.phone,
+                nickName: payload.nickName || state.nickName,
+                todos: payload.todos || state.todos,
+                token: payload.token || state.token,
+            };
+        case userActionsTypes.ADD_USER_TODO:
+            return { ...state, todos: [...state.todos, payload] };
+        case userActionsTypes.DROP_USER_DATA:
+            return { ...defaultState };
+
+        default:
+            return state;
+    }
+};
+
+export default userReducer;
