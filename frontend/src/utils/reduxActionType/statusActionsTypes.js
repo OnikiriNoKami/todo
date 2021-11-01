ce260a7f6b778976a95a975ad181fb361fc41c4b
OnikiriNoKami <butenko.ily@gmail.com>
2021-11-01 21:12:22 +0200

    single status actions

--- frontend/src/utils/reduxActionType/statusActionsTypes.js
+++ frontend/src/utils/reduxActionType/statusActionsTypes.js
@@ -1,5 +1,8 @@
 const statusActionsTypes = {
-
+    SET_STATUS_ID: 'set_status_id',
+    SET_STATUS_TITLE: 'set_status_title',
+    SET_STATUS_DATA: 'set_status_data',
+    DROP_STATUS_DATA: 'drop_status_data',
 }
 
 export default statusActionsTypes;
