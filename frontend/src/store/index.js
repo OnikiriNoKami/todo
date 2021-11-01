018002d2bbfca16ca8a85996132736f8be1176b2
OnikiriNoKami <butenko.ily@gmail.com>
2021-11-01 22:34:52 +0200

    todo reducer import

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,11 +5,13 @@ import thunk from "redux-thunk";
 import paginationReducer from "./pagination";
 import userReducer from "./user";
 import statusReducer from "./status";
+import todoReducer from "./todo";
 
 const combinedReducer = combineReducers({
     pagination: paginationReducer,
     user: userReducer,
     status: statusReducer,
+    todo: todoReducer,
 });
 
 export const store = createStore(
