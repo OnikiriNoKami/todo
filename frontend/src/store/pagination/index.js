2cf85fd0137985d62551e8c066212235f5b2017e
OnikiriNoKami <butenko.ily@gmail.com>
2021-11-01 19:02:54 +0200

    pagination reducer, action creators for todos, comments

---
+++ frontend/src/store/pagination/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import commentPaginationReducer from "./commentPagination";
+import todoPaginationReducer from "./todoPaginaton";
+
+const paginationReducer = combineReducers({
+    todoPagination: todoPaginationReducer,
+    commentPagination: commentPaginationReducer,
+});
+
+export default paginationReducer;
