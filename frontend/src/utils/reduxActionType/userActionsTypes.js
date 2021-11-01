ffe3869a6424c54b5e9e107eadc814bfe56b0414
OnikiriNoKami <butenko.ily@gmail.com>
2021-11-01 19:39:37 +0200

    user action types

---
+++ frontend/src/utils/reduxActionType/userActionsTypes.js
@@ -0,0 +1,12 @@
+const userActionsTypes = {
+    SET_USER_DATA: 'set_user_data',
+    SET_USER_EMAIL: 'set_user_email',
+    SET_USER_NICKNAME: 'set_user_nickName',
+    SET_USER_PHONE: 'set_user_phone',
+    SET_USER_TODOS: 'set_user_todos',
+    SET_USER_TOKEN: 'set_user_token',
+    ADD_USER_TODO: 'add_user_todo',
+    DROP_USER_DATA: 'drop_user_data'
+}
+
+export default userActionsTypes;
