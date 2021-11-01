1198adeeefe0f4911d6d32adf875e152e9de93f6
OnikiriNoKami <butenko.ily@gmail.com>
2021-11-01 22:45:44 +0200

    manyTodos -> todos

--- frontend/src/store/todo/manyTodosReducer.js
+++ frontend/src/store/todo/manyTodosReducer.js
@@ -2,17 +2,17 @@ import { todosActionsTypes } from "../../utils/reduxActionType/todoActionsTypes"
 
 const defaultState= {
     loading: false,
-    manyTodos: [],
+    todos: [],
 }
 
 const manyTodosReducer = (state=defaultState, {type, payload}) => {
     switch(type){
         case todosActionsTypes.SET_TODOS:
-            return { ...state, manyTodos: payload }
+            return { ...state, todos: payload }
         case todosActionsTypes.SET_TODOS_LOADING:
             return { ...state, loading: payload }
         case todosActionsTypes.ADD_TODO:
-            return { ...state, manyTodos: [ ...state.manyTodos, payload ] }
+            return { ...state, manyTodos: [ ...state.todos, payload ] }
         case todosActionsTypes.DROP_TODOS:
             return { ...defaultState }
 
