70b45d4d9d69322e1f90515531c9c39d81cb4e7e
OnikiriNoKami <butenko.ily@gmail.com>
2021-11-02 10:38:41 +0200

    todo querires

--- frontend/src/GraphQL/queries/todoQueries.js
+++ frontend/src/GraphQL/queries/todoQueries.js
@@ -1,9 +1,81 @@
 import { gql } from "@apollo/client";
 
-export const getTodosTest = gql`
+export const getTodos = gql`
     query Queries {
         getTodos {
             _id
+            title
+            description
+            userId
+            statusId
+            beginDate
+            endDate
+        }
+    }
+`;
+
+export const getTodosWithUser = gql`
+    query Queries {
+        getTodos {
+            _id
+            title
+            description
+            statusId
+            userId
+            beginDate
+            endDate
+            user {
+                _id
+                email
+                phone
+                nickName
+                todos
+            }
+        }
+    }
+`;
+
+export const getTodoById = gql`
+    query Queries($todoId: String!) {
+        getTodoById(id: $todoId) {
+            _id
+            title
+            description
+            userId
+            statusId
+            beginDate
+            endDate
+        }
+    }
+`;
+
+export const getTodoById = gql`
+    query Queries($todoId: String!) {
+        getTodoById(id: $todoId) {
+            _id
+            title
+            description
+            userId
+            statusId
+            beginDate
+            endDate
+            user {
+                _id
+                email
+                phone
+                nickName
+                todos
+            }
+        }
+    }
+`;
+
+export const getTodoComments = gql`
+    query Queries($todoId: String!) {
+        getTodosComments(todoId: $todoId) {
+            _id
+            body
+            userId
         }
     }
 `;
