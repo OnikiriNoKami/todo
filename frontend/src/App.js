5302b65d7b635b61d6eb4b7953af8f267df9e868
OnikiriNoKami <butenko.ily@gmail.com>
2021-11-02 10:26:22 +0200

    router integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import apolloClient from "./GraphQL";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 
 function App() {
     return (
-        <ApolloProvider client={apolloClient}>
-            <CssBaseline />
-        </ApolloProvider>
+        <BrowserRouter>
+            <ApolloProvider client={apolloClient}>
+                <CssBaseline />
+                <Layout/>
+            </ApolloProvider>
+        </BrowserRouter>
     );
 }
 
---
+++ frontend/src/components/layout/Router.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+
+
+export default function Router() {
+    return (
+        <Switch>
+            
+        </Switch>
+    )
+}
