26334b126b1b5a2da6554fb43e82cab3ff389bc5
OnikiriNoKami <butenko.ily@gmail.com>
2021-11-03 13:29:58 +0200

    nickName and isDefault for data

--- frontend/src/store/registration/registrationActionCreators.js
+++ frontend/src/store/registration/registrationActionCreators.js
@@ -1,16 +1,66 @@
 import registrationActionsTypes from "../../utils/reduxActionType/registrationActionsTypes";
 
-export const regCreateUserRequest = (request) => ({type: registrationActionsTypes.SET_CREATE_USER_REQUEST, payload: request})
-export const regSetEmailValid = (validStatus) => ({type: registrationActionsTypes.SET_EMAIL_VALID, payload: validStatus})
-export const regSetPasswordValid = (validStatus) => ({type: registrationActionsTypes.SET_PASSWORD_VALID, payload: validStatus})
-export const regSetEmailValue = (emailValue) => ({type: registrationActionsTypes.SET_EMAIL_VALUE, payload: emailValue})
-export const regSetPasswordValue = (passwordValue) => ({type: registrationActionsTypes.SET_PASSWORD_VALUE, payload: passwordValue})
-export const regDropData = (confirm=false) => {
-    if(confirm){
-        return ({type: registrationActionsTypes.DROP_REGISTRATION_DATA})
+export const regCreateUserRequest = (request) => ({
+    type: registrationActionsTypes.SET_CREATE_USER_REQUEST,
+    payload: request,
+});
+export const regSetEmailValid = (validStatus) => ({
+    type: registrationActionsTypes.SET_EMAIL_VALID,
+    payload: validStatus,
+});
+export const regSetPasswordValid = (validStatus) => ({
+    type: registrationActionsTypes.SET_PASSWORD_VALID,
+    payload: validStatus,
+});
+export const regSetEmailValue = (emailValue) => ({
+    type: registrationActionsTypes.SET_EMAIL_VALUE,
+    payload: emailValue,
+});
+export const regSetPasswordValue = (passwordValue) => ({
+    type: registrationActionsTypes.SET_PASSWORD_VALUE,
+    payload: passwordValue,
+});
+export const regDropData = (confirm = false) => {
+    if (confirm) {
+        return { type: registrationActionsTypes.DROP_REGISTRATION_DATA };
     }
-    throw new Error("Confirmation for registration data drop not provided.")
-}
-export const regResetInputs = (request) => ({type: registrationActionsTypes.SET_RESET_REQUEST, payload: request})
-export const regSetPasswordDirty = (dirty) => ({type: registrationActionsTypes.SET_PASSWORD_DIRTY, payload: dirty})
-export const regSetEmailDirty = (dirty) => ({type: registrationActionsTypes.SET_EMAIL_DIRTY, payload: dirty})
+    throw new Error("Confirmation for registration data drop not provided.");
+};
+export const regResetInputs = (request) => ({
+    type: registrationActionsTypes.SET_RESET_REQUEST,
+    payload: request,
+});
+export const regSetPasswordDirty = (dirty) => ({
+    type: registrationActionsTypes.SET_PASSWORD_DIRTY,
+    payload: dirty,
+});
+export const regSetEmailDirty = (dirty) => ({
+    type: registrationActionsTypes.SET_EMAIL_DIRTY,
+    payload: dirty,
+});
+
+export const regSetNickNameDirty = (dirty) => ({
+    type: registrationActionsTypes.SET_NICKNAME_DIRTY,
+    payload: dirty,
+});
+export const regSetNickNameValue = (nickNameValue) => ({
+    type: registrationActionsTypes.SET_NICKNAME_VALUE,
+    payload: nickNameValue,
+});
+export const regSetNickNameValid = (validStatus) => ({
+    type: registrationActionsTypes.SET_NICKNAME_VALID,
+    payload: validStatus,
+});
+
+export const regSetNickNameIsDefault = (isDefault) => ({
+    type: registrationActionsTypes.SET_NICKNAME_IS_DEFAULT,
+    payload: isDefault
+})
+export const regSetPasswordIsDefault = (isDefault) => ({
+    type: registrationActionsTypes.SET_PASSWORD_IS_DEFAULT,
+    payload: isDefault
+})
+export const regSetEmailIsDefault = (isDefault) => ({
+    type: registrationActionsTypes.SET_EMAIL_IS_DEFAULT,
+    payload: isDefault
+})
--- frontend/src/store/registration/registrationReducer.js
+++ frontend/src/store/registration/registrationReducer.js
@@ -5,11 +5,19 @@ const defaultState = {
     email: {
         valid: false,
         value: "",
+        isDefault: true,
         dirty: false,
     },
     password: {
         valid: false,
         value: "",
+        isDefault: true,
+        dirty: false,
+    },
+    nickName: {
+        valid: false,
+        value: "",
+        isDefault: true,
         dirty: false,
     },
     resetRequest: false,
@@ -20,13 +28,16 @@ const registrationReducer = (state = defaultState, { type, payload }) => {
         case registrationActionsTypes.SET_CREATE_USER_REQUEST:
             return { ...state, createUserRequest: payload };
         case registrationActionsTypes.SET_RESET_REQUEST:
-            return { ...state, resetRequest: payload }
+            return { ...state, resetRequest: payload };
         case registrationActionsTypes.SET_EMAIL_VALID:
             return { ...state, email: { ...state.email, valid: payload } };
         case registrationActionsTypes.SET_PASSWORD_DIRTY:
-            return { ...state, password: {...state.password, dirty: payload }}
+            return {
+                ...state,
+                password: { ...state.password, dirty: payload },
+            };
         case registrationActionsTypes.SET_EMAIL_DIRTY:
-            return { ...state, email: {...state.email, dirty: payload }}
+            return { ...state, email: { ...state.email, dirty: payload } };
         case registrationActionsTypes.SET_PASSWORD_VALID:
             return {
                 ...state,
@@ -40,7 +51,28 @@ const registrationReducer = (state = defaultState, { type, payload }) => {
         case registrationActionsTypes.SET_EMAIL_VALUE:
             return { ...state, email: { ...state.email, value: payload } };
         case registrationActionsTypes.DROP_REGISTRATION_DATA:
-            return { ...defaultState }
+            return { ...defaultState };
+        case registrationActionsTypes.SET_NICKNAME_VALID:
+            return {
+                ...state,
+                nickName: { ...state.nickName, valid: payload },
+            };
+        case registrationActionsTypes.SET_NICKNAME_VALUE:
+            return {
+                ...state,
+                nickName: { ...state.nickName, value: payload },
+            };
+        case registrationActionsTypes.SET_NICKNAME_DIRTY:
+            return {
+                ...state,
+                nickName: { ...state.nickName, dirty: payload },
+            };
+        case registrationActionsTypes.SET_EMAIL_IS_DEFAULT:
+            return { ...state, email: { ...state.email, isDefault: payload } };
+        case registrationActionsTypes.SET_PASSWORD_IS_DEFAULT:
+            return { ...state, password: { ...state.password, isDefault: payload } };
+        case registrationActionsTypes.SET_NICKNAME_IS_DEFAULT:
+            return { ...state, nickName: { ...state.nickName, isDefault: payload } };
 
         default:
             return state;
