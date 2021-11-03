f341f048bed28d55babf033c6960ef6d8552a43a
OnikiriNoKami <butenko.ily@gmail.com>
2021-11-03 17:43:01 +0200

    user login hook

--- frontend/src/hooks/GraphQL/mutations/userMutations.js
+++ frontend/src/hooks/GraphQL/mutations/userMutations.js
@@ -1,4 +1,4 @@
-import { createUser } from "../../../GraphQL/mutations/userMutations";
+import { createUser, loginUserMut } from "../../../GraphQL/mutations/userMutations";
 import { useMutation } from "@apollo/client";
 
 export const useCreateUser = () => {
@@ -7,6 +7,20 @@ export const useCreateUser = () => {
 
     return {
         createUserMutation,
+        result: {
+            data,
+            loading,
+            error,
+        },
+    };
+};
+
+export const useLoginForUser = () => {
+    const [loginUserMutation, { data, loading, error }] =
+        useMutation(loginUserMut);
+
+    return {
+        loginUserMutation,
         result: {
             data,
             loading,
