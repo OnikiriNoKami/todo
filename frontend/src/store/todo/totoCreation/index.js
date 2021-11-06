c8b07f1c3e93554faf460b3ced7a9844857d9a2b
OnikiriNoKami <butenko.ily@gmail.com>
2021-11-06 16:31:48 +0200

    todo creation inputs reducer import

--- frontend/src/store/todo/totoCreation/index.js
+++ frontend/src/store/todo/totoCreation/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import todoCreationCollapseReducer from "./todoCreationCollapseReducer";
 import todoCreationDateReducer from "./todoCreationDate/todoCreationDateReducer";
 import todoCreationInputsReducer from "./todoCreationInputs/todoCreationInputsReducer";
+import todoCreationRequestReducer from "./todoCreationRequests/todoCreationRequestsReducer";
 
 const todoCreationReducer = combineReducers({
     collapse: todoCreationCollapseReducer,
     date: todoCreationDateReducer,
     inputs: todoCreationInputsReducer,
+    requests: todoCreationRequestReducer,
 })
 
 export default todoCreationReducer;
